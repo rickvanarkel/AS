8f20515687a2983b2236a0b6dd92b9c55c705091
Laura10121 <125878316+Laura10121@users.noreply.github.com>
2023-02-22 16:30:17 +0100

    _road gefixt!

--- cleaning_roads.py
+++ cleaning_roads.py
@@ -16,20 +16,20 @@ import warnings
 import contextily as cx
 import pandas as pd
 warnings.filterwarnings('ignore')
-
-print(AS.df_roads)
-print(AS.df_roads2)
+#
+# print(AS.df_roads)
+# print(AS.df_roads2)
 
 # A map with all the shapefile of Bangladesh
-fig, ax = plt.subplots(figsize=(8,8))
-
-AS.shapefile_bangladesh.plot(ax=ax, edgecolor='white', linewidth=0.3)
-
-cx.add_basemap(ax, source=cx.providers.CartoDB.Positron)
-ax.set_axis_off()
-ax.set_title('The country of Bangladesh')
-
-plt.show()
+# fig, ax = plt.subplots(figsize=(8,8))
+#
+# AS.shapefile_bangladesh.plot(ax=ax, edgecolor='white', linewidth=0.3)
+#
+# cx.add_basemap(ax, source=cx.providers.CartoDB.Positron)
+# ax.set_axis_off()
+# ax.set_title('The country of Bangladesh')
+#
+# plt.show()
 
 #print(AS.df_roads.head(5))
 #print(AS.df_roads2.head(5))
@@ -41,26 +41,36 @@ link_roads2 = './_uncleaned_data/_roads.tcv'
 df_roads = pd.read_csv(link_roads)
 df_roads2 = pd.read_csv(link_roads2, sep='\t', low_memory=False, skiprows=[0], header=None)
 
-print(AS.df_roads['lat'].describe())
+#print(AS.df_roads['lat'].describe())
 
-sns.set(color_codes=True)
-sns.scatterplot(data = AS.df_roads, x=AS.df_roads.index, y='lat')
-plt.show()
+# sns.set(color_codes=True)
+# sns.scatterplot(data = AS.df_roads, x=AS.df_roads.index, y='lat')
+# plt.show()
 
 roadnames = (AS.df_roads['road'].append(AS.df_roads['road'])).unique()
-
-for data_dict in d.values():
-   x = data_dict.keys()
-   y = data_dict.values()
-   plt.scatter(x,y,color=colors.pop())
-
-plt.legend(d.keys())
-plt.show()
 
 rdict = {}
 
 for x in roadnames:
     df = AS.df_roads.loc[AS.df_roads['road'] == x, 'lat']  # maakt df van elke weg apart met lat
-    rdict[x].append(df)
+    rdict[x] = df
 
-print(rdict)
+#print(rdict['N1'])
+
+sns.set(color_codes=True)
+sns.scatterplot(data = rdict['N1'])
+plt.show()
+
+
+
+rdict_long = {}
+
+for x in roadnames:
+    df = AS.df_roads.loc[AS.df_roads['road'] == x, 'long']  # maakt df van elke weg apart met lat
+    rdict_long[x] = df
+
+#print(rdict['N1'])
+
+sns.set(color_codes=True)
+sns.scatterplot(data = rdict_long['N1'])
+plt.show()
