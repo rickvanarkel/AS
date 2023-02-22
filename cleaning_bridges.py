15ced2d597985d1df5e679e467f8d2dea533ded7
sruinaard <125878309+sruinaard@users.noreply.github.com>
2023-02-22 12:53:13 +0100

    maps of bangladesh

--- cleaning_bridges.py
+++ cleaning_bridges.py
@@ -1,4 +1,12 @@
 import main as AS
+import pandas as pd
+import seaborn as sns
+import geopandas as gpd
+import numpy as np
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+import networkx as nx
+import contextily as cx
 
 # AS.df_bridges is the dataset where...
 # AS.df_BMMS is the dataset where...
@@ -6,4 +14,13 @@ import main as AS
 #print(AS.df_bridges.head(5))
 #print(AS.df_BMMS.head(5))
 
+# A map with all the shapefile of Bangladesh
+fig, ax = plt.subplots(figsize=(8,8))
 
+AS.shapefile_bangladesh.plot(ax=ax, edgecolor='white', linewidth=0.3)
+
+cx.add_basemap(ax, source=cx.providers.CartoDB.Positron)
+ax.set_axis_off()
+ax.set_title('The country of Bangladesh')
+
+plt.show()
--- cleaning_roads.py
+++ cleaning_roads.py
@@ -1,4 +1,24 @@
 import main as AS
 
+import pandas as pd
+import seaborn as sns
+import geopandas as gpd
+import numpy as np
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+import networkx as nx
+import contextily as cx
+
 print(AS.df_roads)
 print(AS.df_roads2)
+
+# A map with all the shapefile of Bangladesh
+fig, ax = plt.subplots(figsize=(8,8))
+
+AS.shapefile_bangladesh.plot(ax=ax, edgecolor='white', linewidth=0.3)
+
+cx.add_basemap(ax, source=cx.providers.CartoDB.Positron)
+ax.set_axis_off()
+ax.set_title('The country of Bangladesh')
+
+plt.show()
