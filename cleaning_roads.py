98973434f773969be255b1031f034926cf885e3d
Laura10121 <125878316+Laura10121@users.noreply.github.com>
2023-02-22 15:51:19 +0100

    _road gefixt!

--- cleaning_roads.py
+++ cleaning_roads.py
@@ -1,4 +1,37 @@
 import main as AS
+import seaborn as sns
+import matplotlib.pyplot as plt
+import warnings
+warnings.filterwarnings('ignore')
+#print(AS.df_roads.head(5))
+#print(AS.df_roads2.head(5))
+#AS.df_roads2.to_excel('Rooadstcv.xlsx')
 
-print(AS.df_roads)
-print(AS.df_roads2)
+print(AS.df_roads['lat'].describe())
+sns.set(color_codes=True)
+sns.scatterplot(data = AS.df_roads, x=AS.df_roads.index, y='lat')
+plt.show()
+
+
+roadnames = (AS.df_roads['road'].append(AS.df_roads['road'])).unique()
+
+#print(roadnames)
+
+road_dict = {}
+# for x in roadnames:
+#     print(x)
+
+# for data_dict in d.values():
+#    x = data_dict.keys()
+#    y = data_dict.values()
+#    plt.scatter(x,y,color=colors.pop())
+
+# plt.legend(d.keys())
+# plt.show()
+
+for x in roadnames:
+    df = AS.df_roads.loc[AS.df_roads['road'] == x, 'lat']  # maakt df van elke weg apart met lat
+
+    print(df.describe())
+
+
