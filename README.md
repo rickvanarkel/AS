c21ac82bd0172d0783b9368f037c4d0f09f045ff
superjager1 <daan@jagernet.nl>
2023-02-23 19:15:05 +0100

    Laura uit de brand helpen! 3.5

--- README.md
+++ README.md
@@ -14,6 +14,27 @@ Last updated: 24-02-2023
 
 ## Model usage
 
+### Cleaning Roads
+The code provided appears to be an attempt to clean a dataset containing data about roads in Bangladesh. The code reads in the original dataset from the file main.py, which presumably contains errors that need to be fixed.
+
+The first step in the code is to create a dictionary called rdict that maps each unique road name in the dataset to a Pandas DataFrame containing the longitude and latitude coordinates of each point along that road. This is achieved by looping over the unique road names in the dataset, creating a new DataFrame for each road that contains only the longitude and latitude columns, and adding that DataFrame to the dictionary under the corresponding road name.
+
+The code then goes on to use the Seaborn library to create a scatter plot of the longitude coordinates for one of the roads in the dataset (N1), presumably to visualize any errors in the data. However, since the scatter plot is only showing the longitude coordinates, it is not clear how useful this visualization would be in identifying errors related to latitude coordinates.
+
+The next step in the code is to create an empty Pandas DataFrame called empty_df to hold the cleaned data. The code then loops over each DataFrame in the rdict dictionary and performs the following operations:
+
+1. Calculate the absolute difference between consecutive longitude and latitude values using the diff() method.
+2. Set any longitude or latitude values where the absolute difference is greater than 0.01 to NaN.
+3. Interpolate missing values using the interpolate() method.
+
+The resulting DataFrame with cleaned data is then appended to empty_df. Finally, the cleaned longitude and latitude values in empty_df are used to replace the corresponding values in the original dataset, which is then written to a new file called _clean_roads.csv.
+
+Overall, this code appears to be a reasonable approach for cleaning a dataset containing errors related to longitude and latitude coordinates. However, without more context about the specific errors in the dataset, it is difficult to say whether this approach will be effective in identifying and correcting all of the errors. Additionally, the code could be improved by incorporating error-handling and data validation checks to ensure that the cleaned data is valid and accurate.
+
+
+
+
+
 ## Used data
 
 
--- cleaning_roads.py
+++ cleaning_roads.py
@@ -1,14 +1,8 @@
 import numpy
-
 import main as AS
 import pandas as pd
 import seaborn as sns
-import geopandas as gpd
-import numpy as np
-import matplotlib as mpl
 import matplotlib.pyplot as plt
-import networkx as nx
-import contextily as cx
 import warnings
 warnings.filterwarnings('ignore')
 
@@ -23,8 +17,32 @@ sns.set(color_codes=True)
 sns.scatterplot(data = rdict['N1']['lon'])
 plt.show()
 
-print(AS.df_roads['lon'].describe())
-print(AS.df_roads['lat'].describe())
+clean_df = pd.DataFrame({'lon' : [], 'lat' : []})
+
+for df in rdict.values():
+    df['lon_dif'] = abs(df['lon'].astype(float).diff())
+    df['lat_dif'] = abs(df['lat'].astype(float).diff())
+    df.loc[df['lon_dif'] > 0.01, 'lon'] = numpy.NAN
+    df.loc[df['lat_dif'] > 0.01, 'lat'] = numpy.NAN
+    df = df.interpolate()
+    clean_df = clean_df.append(df)
+print(clean_df)
+
+AS.df_roads.insert(5, 'gap', '')
+AS.df_roads[['lon', 'lat']] = clean_df[['lon', 'lat']]
+AS.df_roads.to_csv('_roads_cleaned.csv', index=False)
+
+sns.set(color_codes=True)
+sns.scatterplot(data = rdict['N1']['lon'])
+plt.show()
+#
+
+# fouten die naast elkaar liggen of begin of eindpunt zijn kunnen niet gefixt worden via deze methode
+
+# overige pogingen om data fixen:
+
+# print(AS.df_roads['lon'].describe())
+# print(AS.df_roads['lat'].describe())
 
 # for key in rdict:
 #     print(key)
@@ -41,10 +59,33 @@ print(AS.df_roads['lat'].describe())
 #         if np.abs(z_score) > threshold:
 #             outliers.append(value)
 
-for df in rdict.values():
-    df['lon_dif'] = abs(df['lon'].astype(float).diff())
-    df['lat_dif'] = abs(df['lat'].astype(float).diff())
-    df.loc[df['lon_dif'] > 0.01, 'lon'] = numpy.NAN
-    df.loc[df['lat_dif'] > 0.01, 'lat'] = numpy.NAN
 
+# def find_outliers(col):
+#
+#     q1 = col.quantile(.25)
+#     q3 = col.quantile(.75)
+#     IQR = q3 - q1
+#     ll = q1 - (1.5*IQR)
+#     ul = q3 + (1.5*IQR)
+#     upper_outliers = col[col > ul].index.tolist()
+#     lower_outliers = col[col < ll].index.tolist()
+#     bad_indices = list(set(upper_outliers + lower_outliers))
+#     return(bad_indices)
+# #
+# def outlier_treatment(datacolumn):
+#     sorted(datacolumn)
+#     Q1,Q3 = np.percentile(datacolumn , [25,75])
+#     IQR = Q3 - Q1
+#     lower_range = Q1 - (1.5 * IQR)
+#     upper_range = Q3 + (1.5 * IQR)
+#     return lower_range, upper_range
+
+
+# for df in rdict.values():
+#     bad_indexes = []
+#     for col in df.columns:
+#         #print(df[col])
+#         lowerbound,upperbound = outlier_treatment(df[col])
+#         outliers = df[(df[col] < lowerbound) | (df[col] > upperbound)]
+#         print(f' These are the {outliers}')
     #print(df)
