6aa3634adb68c47923024851c5478cb3efd4db83
sruinaard <125878309+sruinaard@users.noreply.github.com>
2023-02-24 17:53:48 +0100

    bridge analysis

--- cleaning_bridges.py
+++ cleaning_bridges.py
@@ -13,6 +13,7 @@ link_close_road = './_uncleaned_data/bridges from GIS/bridges_near_osmroads.csv'
 
 # Cleaned lrp info
 link_lrpinfo = './_uncleaned_data/Roads_InfoAboutEachLRP.csv'
+#link_lrpinfo = './Roads_InfoAboutEachLRP.csv'
 
 # creating pd dataframes
 df_bridges = pd.read_csv(link_bridges, sep=';')
@@ -119,8 +120,139 @@ df_BMMS_definitive = df_BMMS
 print(df_BMMS_definitive.head(5))
 
 # Exporting the dataframe to the original data type
+import pandas as pd
+import geopandas as gpd
+import numpy as np
+import matplotlib.pyplot as plt
+from shapely.geometry import Point
 
+link_bridges = './_uncleaned_data/Bridges_simplesheet.csv'
+link_BMMS = './_uncleaned_data/BMMS_overview.xlsx'
 
+# Obtained from GIS preprocessing
+link_in_country = './_uncleaned_data/bridges from GIS/bridges_in_bangladesh.csv'
+link_close_road = './_uncleaned_data/bridges from GIS/bridges_near_osmroads.csv'
+
+# Cleaned lrp info
+link_lrpinfo = './_uncleaned_data/Roads_InfoAboutEachLRP.csv'
+
+# creating pd dataframes
+df_bridges = pd.read_csv(link_bridges, sep=';')
+df_BMMS = pd.read_excel(link_BMMS)
+df_bridges_in_country = pd.read_csv(link_in_country)
+df_bridges_near_roads = pd.read_csv(link_close_road)
+df_lrp_info = pd.read_csv(link_lrpinfo)
+
+# Check for missing values
+print(df_BMMS.isnull().sum())
+print(df_bridges.isnull().sum())
+
+# Creating a dataframe with the essential information
+BMMS_lonlat = ['road','LRPName', 'lat', 'lon']
+df_BMMS_lonlat = df_BMMS.loc[:, BMMS_lonlat]
+
+# Check if coordinates are swapped, they are not (eventually not needed)
+print(df_BMMS_lonlat.sort_values('lat', ascending=False).head(5))
+
+# Create bridge id
+df_BMMS['bridge_id'] = df_BMMS['road'] + df_BMMS['LRPName']
+
+# Some checks
+print(df_BMMS_lonlat.nunique())
+print(df_BMMS_lonlat.isnull().sum())
+
+# Making bridge and road id's
+df_BMMS_lonlat['bridge_id'] = df_BMMS_lonlat['road'] + df_BMMS_lonlat['LRPName']
+df_bridges_near_roads['bridge_id']= df_bridges_near_roads['road'] + df_bridges_in_country['LRPName']
+df_bridges_in_country['bridge_id'] = df_bridges_in_country['road'] + df_bridges_in_country['LRPName']
+df_lrp_info['road_id'] = df_lrp_info['road'] + df_lrp_info['lrp']
+
+# Adding information about the criteria to the dataframes
+df_BMMS_lonlat['outside country'] = True
+for x in df_bridges_in_country['bridge_id']:
+    if df_BMMS_lonlat['bridge_id'].isin([x]).any():
+        df_BMMS_lonlat.loc[(df_BMMS_lonlat['bridge_id'] == x), 'outside country'] = False
+print(f'Number of points outside of the country: {df_BMMS_lonlat["outside country"].value_counts()[True]}')
+
+df_BMMS_lonlat['far from road'] = True
+for x in df_bridges_near_roads['bridge_id']:
+    if df_BMMS_lonlat['bridge_id'].isin([x]).any():
+        df_BMMS_lonlat.loc[df_BMMS_lonlat['bridge_id'] == x, 'far from road'] = False
+print(f'Number of points far from road: {df_BMMS_lonlat["far from road"].value_counts()[True]}')
+
+# Set lon and lat to NaN if the criteria is false
+df_BMMS_lonlat.loc[df_BMMS_lonlat['outside country'], ['lon', 'lat']] = np.nan
+df_BMMS_lonlat.loc[df_BMMS_lonlat['far from road'], ['lon', 'lat']] = np.nan
+
+# Dataframe for visualisation
+df_BMMS_lonlat_nan = df_BMMS_lonlat
+
+# A check on the missing values
+print(df_BMMS_lonlat.isnull().sum())
+
+# Collecting wrong bridge id's
+wrongbridgeid = []
+for x in df_lrp_info['road_id']:
+    if df_BMMS_lonlat['bridge_id'].isin([x]).any():
+        pass
+    else:
+        wrongbridgeid.append(x)
+
+print(df_lrp_info['road_id'].nunique())
+print(df_BMMS_lonlat['bridge_id'].nunique())
+
+# Create a boolean mask for rows with missing lat and lon values
+mask = df_BMMS_lonlat['lat'].isnull() & df_BMMS_lonlat['lon'].isnull()
+
+# Loop over the road IDs in df_lrp_info
+for x in df_lrp_info['road_id']:
+    # Check if x matches any bridge IDs in df_BMMS_lonlat
+    if df_BMMS_lonlat['bridge_id'].isin([x]).any():
+        # Get the lat and lon values from df_lrp_info for this road ID
+        lat, lon = df_lrp_info.loc[df_lrp_info['road_id'] == x, ['lat', 'lon']].values[0]
+
+        # Update the lat and lon values in df_BMMS_lonlat where the mask is True
+        df_BMMS_lonlat.loc[mask & (df_BMMS_lonlat['bridge_id'] == x), ['lat', 'lon']] = [lat, lon]
+
+print(df_BMMS_lonlat.isnull().sum())
+
+# Prepare for plotting
+df_BMMS_plotting = df_BMMS
+df_BMMS_plotting['geometry'] = [Point (xy) for xy in zip(df_BMMS_plotting['lon'], df_BMMS_plotting['lat'])]
+gdf_BMMS_plotting = gpd.GeoDataFrame(df_BMMS_plotting, crs="EPSG:3857") # crs="EPSG:3857"
+
+df_BMMS_lonlat['geometry'] = [Point (xy) for xy in zip(df_BMMS_lonlat['lon'], df_BMMS_lonlat['lat'])]
+gdf_BMMS_lonlat = gpd.GeoDataFrame(df_BMMS_lonlat, crs="EPSG:3857") # crs="EPSG:3857"
+
+df_BMMS_lonlat_nan['geometry'] = [Point (xy) for xy in zip(df_BMMS_lonlat_nan['lon'], df_BMMS_lonlat_nan['lat'])]
+gdf_BMMS_lonlat_nan = gpd.GeoDataFrame(df_BMMS_lonlat_nan, crs="EPSG:3857") # crs="EPSG:3857"
+
+# Plots for examining the lon and lat
+fig, ax = plt.subplots(figsize=(8,8))
+#gdf_BMMS_plotting.plot(ax=ax, cmap='Reds') # old coordinates
+gdf_BMMS_lonlat.plot(ax=ax, cmap='Blues') # new coordinates
+#gdf_BMMS_lonlat_nan.plot(ax=ax, cmap='Greens') # before new coordinates
+plt.show()
+
+# Updating the original dataframe
+print(df_BMMS.columns.values.tolist())
+print(df_BMMS_lonlat.columns.values.tolist())
+
+print(df_BMMS.shape)
+print(df_BMMS_lonlat.shape)
+
+# A quick method to merge the two files. Merge and dropping columns was also possible
+# The indexing stayed the same since we did not change order, and the shapes are the same
+df_BMMS_definitive = df_BMMS
+df_BMMS_definitive['lat'] = df_BMMS_lonlat['lat']
+df_BMMS_definitive['lon'] = df_BMMS_lonlat['lon']
+
+print(df_BMMS_definitive.head(5))
+
+# Exporting the dataframe to the original data type
+df_BMMS_definitive.to_excel('BMMS_overview.xlsx', index=False)
+
+# Some trial and error under this!
 '''
 # Our try to do the GIS analyses with Geopandas, but the projection of the points did not work (points on the figure vs geopoints on the map)
 
